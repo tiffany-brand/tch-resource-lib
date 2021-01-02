06affce952d32178ff7665b75dd58a16a8e17fac
Tiffany Brand <brand2learn@gmail.com>
2021-01-02 10:56:17 -0500

    Connect to db

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
+require('dotenv').config()
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+// connect to Mongo database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tchresource', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
