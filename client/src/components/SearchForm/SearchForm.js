81241d3514ee092e90f9d3b9de80702bf60d2312
Tiffany Brand <brand2learn@gmail.com>
2021-02-17 21:54:13 -0500

    Create Search Form w basic styling

---
+++ client/src/components/SearchForm/SearchForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { TextField, Grid, Button, Typography, Container } from '@material-ui/core';
+import useStyles from './styles';
+
+const SearchForm = () => {
+
+    const classes = useStyles();
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    // Handles updating component state when the user types into the input field
+    const handleInputChange = event => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(searchTerm.split(" ").join("+").trim())
+    };
+
+    return (
+        <Container className={classes.root}>
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={10}>
+                        <TextField className={classes.input} id="standard-search" label="search" type="search" name="search" variant="outlined" onChange={handleInputChange} />
+                    </Grid>
+                    <Grid item xs={12} sm={2}>
+                        <Button className={classes.buttons} variant="contained" color="secondary" type="submit">Search</Button>
+                    </Grid>
+                </Grid>
+
+            </form>
+        </Container>
+    )
+
+}
+
+export default SearchForm;
