eb120e8b3d5f75937954dcdcb3543005c6a97c7f
Tiffany Brand <brand2learn@gmail.com>
2021-02-17 21:58:42 -0500

    Update Search view spacing

--- client/src/views/Search/Search.js
+++ client/src/views/Search/Search.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react'
 import ResourceList from '../../components/ResourceList/ResourceList';
 import SearchForm from '../../components/SearchForm/SearchForm';
 import { Container, Typography } from '@material-ui/core';
+import useStyles from './styles';
 
 import { getResources, searchResources } from '../../actions/resources';
 import { useDispatch } from 'react-redux';
 
 const Search = () => {
 
+    const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -17,7 +19,7 @@ const Search = () => {
     }, [dispatch]);
 
     return (
-        <Container>
+        <Container className={classes.root}>
             <Typography gutterBottom variant="h6">Search for Resources</Typography>
             <SearchForm />
             <ResourceList />
